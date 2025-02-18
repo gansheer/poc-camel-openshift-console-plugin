d442d1f95d61751ead9834373a448d7aff70d55c
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-02-18 16:41:48 +0100

    annotations in const

--- plugin/src/const.ts
+++ plugin/src/const.ts
@@ -1,1 +1,15 @@
 export const FLAG_OPENSHIFT_CAMEL = 'OPENSHIFT_CAMEL';
+
+
+export const METADATA_ANNOTATION_APP_VERSION = 'app.kubernetes.io/version';
+
+export const METADATA_ANNOTATION_CAMEL_VERSION = 'camel/camel-core-version';
+
+export const METADATA_ANNOTATION_CAMEL_QUARKUS_PLATFORM_VERSION = 'camel/quarkus-platform';
+export const METADATA_ANNOTATION_CAMEL_CEQ_VERSION = 'camel/camel-quarkus';
+
+export const METADATA_ANNOTATION_QUARKUS_APPS = 'app.quarkus.io/';
+export const METADATA_ANNOTATION_QUARKUS_BUILD_TIMESTAMP = METADATA_ANNOTATION_QUARKUS_APPS + 'build-timestamp';
+
+export const METADATA_ANNOTATION_CAMEL_SPRINGBOOT_VERSION = 'camel/spring-boot-version';
+export const METADATA_ANNOTATION_CAMEL_CSB_VERSION = 'camel/camel-spring-boot-version';
